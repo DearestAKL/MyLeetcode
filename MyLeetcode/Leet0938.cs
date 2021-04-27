16ed835ace8fccbf529e635a76081fec67b8fd56
Akari <149439786@qq.com>
2021-04-27 20:01:53 +0800

    1011

---
+++ MyLeetcode/Leet0938.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+// 938. 二叉搜索树的范围和
+
+/*
+给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+
+示例 1：
+输入：root = [10,5,15,3,7,null,18], low = 7, high = 15
+输出：32
+
+示例 2：
+输入：root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+输出：23
+ 
+提示：
+树中节点数目在范围 [1, 2 * 104] 内
+1 <= Node.val <= 105
+1 <= low <= high <= 105
+所有 Node.val 互不相同
+ */
+
+
+public class Leet0938
+{
+    public int RangeSumBST(TreeNode root, int low, int high)
+    {
+
+        if (root == null)
+        {
+            return 0;
+        }
+
+        if (root.val > high)
+        {
+            return RangeSumBST(root.left, low, high);
+        }
+        else if (root.val < low)
+        {
+            return RangeSumBST(root.right, low, high);
+        }
+        else
+        {
+            return root.val + RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high);
+        }
+    }
+
+}
